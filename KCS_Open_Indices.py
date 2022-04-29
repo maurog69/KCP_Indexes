c511272a761e2cd0fc47d720d0c6e27f641faa83
Mauricio Gomez <maurog@hotmail.com>
2022-04-29 15:48:32 -0500

    "Let user input the directory"

--- KCS_Open_Indices.py
+++ KCS_Open_Indices.py
@@ -1,21 +1,36 @@
 """
-Make a walk thru C:\Alaris\TempImages
+Make a list of jobs inside the Temporal Directory
+Normally is C:\ScanPro
+I have it in C:\Alaris\TempImages
 """
 
 import os
 
-directory = r"C:\Alaris\TempImages"
 
+user_input = input("Please write the Temporal Directory or hit Enter for default: ")
+
+if user_input == "":
+    temp_kcp_directory = r"C:\Alaris\TempImages"
+else:
+    if os.path.isdir(user_input):
+        temp_kcp_directory = user_input
+    else:
+        print("Please write a valid path")
+        exit()
+
+
+# Make a list of the directories below 
 list_of_KCP_jobs = []
 
-for filename in os.listdir(directory):
-    file_path = os.path.join(directory, filename)
+for filename in os.listdir(temp_kcp_directory):
+    file_path = os.path.join(temp_kcp_directory, filename)
     
     if os.path.isdir(file_path):    # checking if is a directory
-        list_of_KCP_jobs.append(file_path)
+        list_of_KCP_jobs.append(filename)
 
 count_of_jobs = 1
 for job in list_of_KCP_jobs:
+    print("These are the available jobs:") 
     print("Job Number:", count_of_jobs, "Name:", job)
     count_of_jobs += 1
 
