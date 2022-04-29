f76cd8557a7e8fb6a32e906902807d2ad2738dff
Mauricio Gomez <maurog@hotmail.com>
2022-04-29 18:20:54 -0500

    After selecting the job, list the documents inside the job

--- KCS_Open_Indices.py
+++ KCS_Open_Indices.py
@@ -6,26 +6,36 @@ I have it in C:\Alaris\TempImages
 
 import os
 
+
+def list_of_directories(path):
+    """
+    Returns a list of the directories below "path"
+    """
+    list_of_directories = []
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+    
+        if os.path.isdir(file_path):    # checking if is a directory
+            list_of_directories.append(filename)
+    return list_of_directories
+
+
+
+
 user_input = input("Please write the Temporal Directory or hit Enter for default: ")
 
 if user_input == "":
-    temp_kcp_directory = r"C:\Alaris\TempImages"
+    job_directory = r"C:\Alaris\TempImages"
 else:
     if os.path.isdir(user_input):
-        temp_kcp_directory = user_input
+        job_directory = user_input
     else:
         print("Please write a valid path")
         exit()
 
 
-# Make a list of the directories below "temp_kcp_directory"
-list_of_KCP_jobs = []
-for filename in os.listdir(temp_kcp_directory):
-    file_path = os.path.join(temp_kcp_directory, filename)
-    
-    if os.path.isdir(file_path):    # checking if is a directory
-        list_of_KCP_jobs.append(filename)
 
+list_of_KCP_jobs = list_of_directories(job_directory)
 print("These are the available jobs:") 
 
 count_of_jobs = 0
@@ -33,16 +43,21 @@ for job in list_of_KCP_jobs:
     print("Job Number:", count_of_jobs+1, "Name:", job)
     count_of_jobs += 1
 
+
 # Ask the user for a job to scan
 prompt = "Please select a job number or enter to exit (1 - " + str(count_of_jobs) + "): "
 while True:
     user_input = input(prompt)
     if user_input == "":
         exit()
-    if user_input.isdigit():
+    if user_input.isdigit() and (1 <= int(user_input) <= count_of_jobs):
         break
 
 selected_job_number = int(user_input)
-print("You have selected job ", user_input, " ", list_of_KCP_jobs[selected_job_number])
 
+scan_directory = os.path.join(job_directory, list_of_KCP_jobs[selected_job_number-1])
 
+list_of_documents = list_of_directories(scan_directory)
+
+print("\nYou have selected job ", user_input, ", ", list_of_KCP_jobs[selected_job_number-1], \
+    ", with ", len(list_of_documents), " documents.\n", sep="")
